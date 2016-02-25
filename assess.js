f29ee87f6303cfd2045df0537b8eb8b1a684513d
Kedar <kedarmail@gmail.com>
2016-02-25 09:03:08 -0500

    initial commit, published to npm repository

---
+++ assess.js
@@ -0,0 +1,12 @@
+const assessor = require('workshopper')
+
+assessor({
+    name: 'student_assess',
+    title: 'AssessMe',
+    subtitle: 'Assess me please',
+    appDir: __dirname,
+    menu: {
+        bg: 'green'
+    }
+})
+
---
+++ exercises/all_your_base/exercise.js
@@ -0,0 +1,10 @@
+const exercise      = require('workshopper-exercise')(),
+    filecheck       = require('workshopper-exercise/filecheck'),
+    execute         = require('workshopper-exercise/execute'),
+    comparestdout   = require('workshopper-exercise/comparestdout')
+
+exercise = filecheck(exercise)
+exercise = execute(exercise)
+exercise = comparestdout(exercise)
+
+module.exports = exercise
