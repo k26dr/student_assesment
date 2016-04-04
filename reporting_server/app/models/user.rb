1ab54e68fbdd1c8a32ad28ef6292be2a86171443
Kedar Iyer <kedarmail@gmail.com>
2016-04-04 16:32:14 -0700

    wipe clean, new slate

--- reporting_server/app/models/user.rb
+++
@@ -1,6 +0,0 @@
-class User < ActiveRecord::Base
-    validates :username, :presence => true, :uniqueness => true, :length => { :in => 3..20 }
-    validates :password, presence: true, length: { minimum: 6 }
-    has_secure_password
-end
-
