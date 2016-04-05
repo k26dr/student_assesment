0cff405086e646172177993b905c1d580ef6178f
Kedar Iyer <kedarmail@gmail.com>
2016-04-04 17:15:20 -0700

    ruby assessment and test

---
+++ assess.rb
@@ -0,0 +1,36 @@
+require "net/http"
+require "uri"
+
+@tests = 0
+@tests_failed = 0
+@tests_passed = 0 
+@name = "NO_NAME_PROVIDED"
+@assignment = -1
+@server_url = "http://localhost:8000"
+
+def assert statement
+    # get line number of test
+    stack = caller.to_s
+    first_index = stack.index(':') + 1
+    length = stack.index(':', first_index) - first_index
+    line = stack[first_index, length]
+
+    @tests += 1
+    if statement
+        @tests_passed += 1
+        puts "Test #{@tests} on line #{line} \033[32mPass\033[0m"
+    else
+        @tests_failed += 1
+        puts "Test #{@tests} on line #{line} \033[31mFail\033[0m"
+    end
+end
+
+def report
+    uri = URI.parse(@server_url)
+    Net::HTTP.post_form(uri, {
+        "student" => @name,
+        "assignment" => @assignment,
+        "tests_passed" => @tests_passed
+    })
+end
+    
---
+++ test.rb
@@ -0,0 +1,8 @@
+require './assess.rb'
+@assignment = 1
+@name = "Kedar"
+
+assert true
+assert false
+
+report
